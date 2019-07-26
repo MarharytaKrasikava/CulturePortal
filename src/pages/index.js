ff439432bdf8ebd84f6b4ee66ca9338975139f25
MarharytaKrasikava <marharytakrasikava@gmail.com>
2019-07-26 18:03:06 +0300

    reverting to working moment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,3 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby blog with Markdown pages.</p>
-    <Link to="/blog/jury-chashchavacki/">Go to my first Markdown blog post</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default () => <div>Hello world!</div>
